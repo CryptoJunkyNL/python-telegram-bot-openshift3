86e23f642011e0cdbc6f5d940683d78f17ebba60
CryptoJunkyNL <35580044+CryptoJunkyNL@users.noreply.github.com>
2018-01-20 19:50:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Fi
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = 'YOUR-TELEGRAM-BOT-TOKEN'
+TOKEN = '514912732:AAEuvLKQTDbTS6JpYGw9yZ6nl2VZfZ1UE4k'
 
 
 def start(bot, update):
